56fe4eb258c357dbed390400f242a676bfa3e2e1
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-10 22:43:03 +0300

    task over

--- 01-drum-machine/index.js
+++ 01-drum-machine/index.js
@@ -1,12 +1,26 @@
-// catch every key press of keyboard
-window.addEventListener('keydown', function(e) {
-  // audio DOM
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  //keys DOM
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  if (!audio) return; // stop the undefined keys
-  
-  audio.currentTime = 0; // rewind from 0(play instantly when re press to the button )  
-  audio.play(); // invoke the function
-})
+function playSound(e) {
+  // audio DOM
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   //keys DOM
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   if (!audio) return; // stop the undefined keys
+   audio.currentTime = 0; // rewind from 0(play instantly when re press to the button )  
+   audio.play(); // invoke the function
+
+   // add the playing class 
+   key.classList.add('playing');  
+}
+
+function removeTransition(e) {
+if (e.propertyName !== 'transform') return; // skip other classes than transform (on elements styles)
+this.classList.remove('playing');
+}
+
+// catch keys on dom and when transition ends remove class
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+// catch every key press of keyboard and run playSound function
+window.addEventListener('keydown', playSound);
