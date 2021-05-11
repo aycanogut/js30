890e7a1e60beb682122b32dc5c12fe7fb097cb2e
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-11 20:54:40 +0300

    hour and minute constants declared to setDate function]

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -4,11 +4,11 @@ const secondHand = document.querySelector('.hand-second');
 
 function setDate() {
   const time = new Date();
+  const hour = time.getHours()
+  const minute = time.getMinutes()
   const sec = time.getSeconds();
   const secondsDeg = ((sec / 60 ) * 360 + 90);
   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-  
-  console.log(sec)
-}
+  }
 
 setInterval(setDate, 1000);
