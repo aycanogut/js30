ac48a16663e4456049a0cc8d6e72fc08b66f8c73
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-11 20:50:41 +0300

    setDate function created

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -2,3 +2,5 @@ const hourHand = document.querySelector('.hand-hour');
 const minuteHand = document.querySelector('.hand-minute');
 const secondHand = document.querySelector('.hand-second');
 
+function setDate() {
+}
