496f9d5ce430a246e8181670ea3ebf239b7ecaaf
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-11 20:51:07 +0300

    constant declared and second is functional

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -3,4 +3,12 @@ const minuteHand = document.querySelector('.hand-minute');
 const secondHand = document.querySelector('.hand-second');
 
 function setDate() {
+  const time = new Date();
+  const sec = time.getSeconds();
+  const secondsDeg = ((sec / 60 ) * 360 + 90);
+  secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+  
+  console.log(sec)
 }
+
+setInterval(setDate, 1000);
