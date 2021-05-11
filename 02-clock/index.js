d671a8e9cb2115f1ade7cbd9bcb6fe1c1199a3ad
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-11 21:07:37 +0300

    hour and minute constants and transform styles declared

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -4,11 +4,20 @@ const secondHand = document.querySelector('.hand-second');
 
 function setDate() {
   const time = new Date();
+
   const hour = time.getHours()
   const minute = time.getMinutes()
   const sec = time.getSeconds();
+
+  const hoursDeg = (( hour / 12) * 360 + 90);  
+  const minutesDeg = (( minute / 60) * 360 + 90);  
   const secondsDeg = ((sec / 60 ) * 360 + 90);
+
+  hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+
+  console.log(minute)
   }
 
 setInterval(setDate, 1000);
