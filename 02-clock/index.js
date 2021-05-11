ed2c9c1b99734058ccf84f1dd110bac99cb15969
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-11 21:15:00 +0300

    comments added to index.js

--- 02-clock/index.js
+++ 02-clock/index.js
@@ -1,23 +1,24 @@
+// dom declarations
 const hourHand = document.querySelector('.hand-hour');
 const minuteHand = document.querySelector('.hand-minute');
 const secondHand = document.querySelector('.hand-second');
 
 function setDate() {
+  // time constants
   const time = new Date();
-
   const hour = time.getHours()
   const minute = time.getMinutes()
   const sec = time.getSeconds();
 
+  // calculate the aspects of rotation
   const hoursDeg = (( hour / 12) * 360 + 90);  
   const minutesDeg = (( minute / 60) * 360 + 90);  
   const secondsDeg = ((sec / 60 ) * 360 + 90);
 
+  // declare that rotations to elements each second
   hourHand.style.transform = `rotate(${hoursDeg}deg)`;
   minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-
-  console.log(minute)
   }
 
 setInterval(setDate, 1000);
